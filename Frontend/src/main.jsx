de40d9a462a5d34795392225747d3cc27fe3e8d5
khushivermaa12 <khushiverm12@gmail.com>
2026-07-10 22:59:14 +0530

    apply Kanit font for Thai language

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,9 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './i18n/i18n'
+import { getLangFromUrl } from './utils/langQuery'
+
+document.documentElement.lang = getLangFromUrl('en')
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
